590b853fb42fc32349d4888a71d1c10f3eaff18c
ShabanKamel <sh3ban.kamel@gmail.com>
2019-11-10 16:39:11 +0200

    optimize code

--- feature/head/src/main/java/tracker/feature/head/AppHead.kt
+++ feature/head/src/main/java/tracker/feature/head/AppHead.kt
@@ -3,7 +3,6 @@ package tracker.feature.head
 import android.content.Intent
 import androidx.fragment.app.FragmentActivity
 import com.sha.apphead.AppHead
-import com.sha.apphead.BadgeView
 import com.sha.apphead.Head
 import com.sha.apphead.HeadView
 import com.sha.kamel.navigator.modular.ActivityModuleNavigator
@@ -15,20 +14,18 @@ object AppHead {
 
     @JvmStatic
     fun show(activity: FragmentActivity) {
-        runCatching {
-            val builder = Head.Builder(R.mipmap.ic_launcher)
-                    .headView(HeadView.Args()
-                            .onClick {
-                                ActivityModuleNavigator(activity, activity.packageName)
-                                        .withFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                        .navigate(Activities.Main)
-                            }
-                            .setupImage {
-                                it.layoutParams.width = 200
-                                it.layoutParams.height = 200
-                            })
-            AppHead(builder).show(activity)
-        }.onFailure { it.printStackTrace() }
+        val builder = Head.Builder(R.mipmap.ic_launcher)
+                .headView(HeadView.Args()
+                        .onClick {
+                            ActivityModuleNavigator(activity, activity.packageName)
+                                    .withFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    .navigate(Activities.Main)
+                        }
+                        .setupImage {
+                            it.layoutParams.width = 200
+                            it.layoutParams.height = 200
+                        })
+        AppHead(builder).show(activity)
     }
 
     fun hide() {
